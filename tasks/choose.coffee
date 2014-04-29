eeff5a0f8a706ab42331384d8a795d6f06978498
Leny <info@flatland.be>
2014-04-29 04:05:54 +0200

    project scaffolding

---
+++ tasks/choose.coffee
@@ -0,0 +1,15 @@
+###
+ * grunt-choose
+ * https://github.com/Leny/grunt-choose
+ *
+ * Copyright (c) 2014 Leny
+ * Licensed under the MIT license.
+###
+
+"use strict"
+
+module.exports = ( grunt ) ->
+
+    grunt.registerMultiTask "choose", "Ask the user to choose a task to run in a list.", ->
+
+        grunt.log.writeln "There's many things TODO here."
---
+++ tasks/choose.js
@@ -0,0 +1,18 @@
+
+/*
+ * grunt-choose
+ * https://github.com/Leny/grunt-choose
+ *
+ * Copyright (c) 2014 Leny
+ * Licensed under the MIT license.
+ */
+
+(function() {
+  "use strict";
+  module.exports = function(grunt) {
+    return grunt.registerMultiTask('choose', 'Ask the user to choose a task to run in a list.', function() {
+      return grunt.log.writeln("There's many things TODO here.");
+    });
+  };
+
+}).call(this);
